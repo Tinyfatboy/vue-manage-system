9465948bb0694a34a56e0af6da14aa12b63514c5
tinyfatboy <tzhang015@e.ntu.edu.sg>
2021-04-15 23:20:17 +0800

    update(pages): update the login and register page

---
+++ src/views/login.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="login">
+    <el-container>
+      <el-main>
+        <el-form
+          :label-position="labelPosition"
+          label-width="80px"
+          :model="loginForm"
+        >
+          <h2>通航用户登录</h2>
+          <el-form-item label="用户名：">
+            <el-input
+              v-model="loginForm.userName"
+              placeholder="请输入账号(手机号)"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="密 码：">
+            <el-input
+              v-model="loginForm.password"
+              placeholder="请输入密码"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button class="login-btn" type="primary"> 登录 </el-button>
+          </el-form-item>
+          <el-button class="register-btn" type="text"> 注册 </el-button>
+        </el-form>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      labelPosition: 'left',
+      loginForm: {
+        userName: '',
+        password: ''
+      },
+      loading: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.el-container {
+  height: 100vh;
+}
+.el-main {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.el-form {
+  border: 1px solid black;
+  padding: 25px;
+  min-width: 400px;
+  position: relative;
+}
+.el-form > h2 {
+  width: 100%;
+  text-align: center;
+}
+.login-btn {
+  width: 140px;
+  margin-left: 60px;
+  margin-top: 10px;
+}
+.register-btn {
+  position: absolute;
+  right: 25px;
+  bottom: 85px;
+}
+</style>
---
+++ src/views/register.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="register">
+    <el-container>
+      <el-header>
+        <h2>注册</h2>
+      </el-header>
+      <el-main>
+        <el-form
+          :label-position="labelPosition"
+          label-width="100px"
+          :model="signUpForm"
+        >
+          <el-form-item label="账号：">
+            <el-input
+              size="small"
+              v-model="loginForm.userName"
+              placeholder="请输入您的手机号"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="密码：">
+            <el-input
+              size="small"
+              v-model="loginForm.password"
+              placeholder="请输入密码"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="密码重复：">
+            <el-input
+              size="small"
+              v-model="loginForm.userName"
+              placeholder="请再次输入密码"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="邮箱：">
+            <el-input
+              size="small"
+              v-model="loginForm.userName"
+              placeholder="请输入您的邮箱"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="姓名：">
+            <el-input
+              v-model="loginForm.userName"
+              placeholder="请输入您的姓名"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="身份证号：">
+            <el-input
+              size="small"
+              v-model="loginForm.userName"
+              placeholder="请输入您的身份证号"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="所属公司：">
+            <el-input
+              size="small"
+              v-model="loginForm.userName"
+              placeholder="请输入您所属的公司"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button class="register-btn" type="primary"> 注册 </el-button>
+          </el-form-item>
+        </el-form>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'register',
+  data () {
+    return {
+      labelPosition: 'right',
+      loginForm: {
+        userName: '',
+        password: ''
+      },
+      loading: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.el-container {
+  height: 100vh;
+}
+.el-header > h2 {
+  padding-left: 60px;
+}
+.el-main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: -60px;
+}
+.el-form {
+  width: 400px;
+}
+.el-form-item {
+  margin-bottom: 10px;
+}
+.register-btn {
+  margin-top: 10px;
+  margin-left: 40px;
+  width: 140px;
+}
+</style>
